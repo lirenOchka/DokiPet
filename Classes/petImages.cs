77b63ed0cb0aab2997023d9444df7302ee8b7244
liren <161711697+lirenOchka@users.noreply.github.com>
2026-01-19 22:03:11 +0300

    Add files via upload

---
+++ Classes/petImages.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace tamogoji.Classes
+{
+    public class petImages
+    {
+        public string name;
+        public ImageSource active;
+        public ImageSource calmness;
+
+        public petImages(string name, string activePath, string calmnessPath)
+        {
+            this.name = name;
+            this.active = LoadImage(activePath);
+            this.calmness = LoadImage(calmnessPath);
+        }
+
+        private static BitmapImage LoadImage(string path)
+        {
+            var full = Path.GetFullPath(path);
+            var bi = new BitmapImage();
+            bi.BeginInit();
+            bi.UriSource = new Uri(full, UriKind.Absolute);
+            bi.CacheOption = BitmapCacheOption.OnLoad;
+            bi.EndInit();
+            bi.Freeze();
+            return bi;
+        }
+    }
+}
---
+++ Classes/stuleMessangeText.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamogoji.Classes
+{
+    internal enum stuleMessangeText
+    {
+        normal,
+        whisper,
+        scream,
+        bigScream
+    }
+}
